12e9f6deb6a956069b4f9bbeb9555bb85444e954
Guillaume Cartier <gcartier@jazzscheme.org>
2013-06-29 07:45:12 -0400

    Embed settings into devel executables

--- setup/devel-launcher.scm
+++ setup/devel-launcher.scm
@@ -6,24 +6,15 @@
 ;;;
 
 
-(define setup-install
-  (getenv "SETUPINSTALL"))
-
 (define setup-source
   (getenv "SETUPSOURCE"))
 
-
-(define (load-install filename)
-  (let ((file (string-append setup-install "/" filename ".scm")))
-    (load file)))
 
 (define (load-source filename)
   (let ((file (string-append setup-source "/" filename ".scm")))
     (load file)))
 
 
-;; reload settings
-(load-install "settings")
 (load-source "base")
 (load-source "geometry")
 (load-source "color")
