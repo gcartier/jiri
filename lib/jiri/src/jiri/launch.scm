4723c641695facd95f2006133706ec15c814f260
Guillaume Cartier <gcartier@jazzscheme.org>
2014-04-03 17:55:11 -0400

    Update jiri project

--- lib/jiri/src/jiri/launch.scm
+++ lib/jiri/src/jiri/launch.scm
@@ -38,16 +38,7 @@
 (unit jiri.launch
 
 
-(require jiri.windows
-         jiri.git
-         jiri.git-interface
-         jiri.base
-         jiri.geometry
-         jiri.color
-         jiri.font
-         jiri.window
-         jiri.draw
-         jiri.view
+(require jiri.base
          jiri.structure
          jiri.work)
 
